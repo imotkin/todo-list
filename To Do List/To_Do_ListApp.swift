f56a7734c0775cc6e77a014a6b48542cb0666cd6
Ilya Motkin <imotkin@outlook.com>
2025-02-18 13:12:27 +0300

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Ilya Motkin on 18.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
